a76882adee8a23037475171ba404c8455b9a06dc
tobi turner <tobi@something.com>
2022-11-18 08:21:49 +1100

    added card, fixed tailwind again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from "react";
 function App() {
     return (
         <>
-            <h1 className="text-3xl">Hello world</h1>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+                <h1 className="text-3xl">hi</h1>
+            </div>
         </>
     );
 }
