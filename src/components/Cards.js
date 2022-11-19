53281624bc667f6fcadd031154529cca3b4fa036
tobi turner <tobi@something.com>
2022-11-19 17:25:49 +1100

    added website button, fixed tags dependency to depend on props.pic change instead

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,15 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const Cards = (props) => {
   const [tags, setTags] = useState([]);
   useEffect(() => {
     setTags(props.pic.tags.split(", "));
-  }, [tags])
+  }, [props.pic]);
   return (
     <div className="max-w-sm rounded bg-white shadow-lg flex flex-col justify-between">
       <img src={props.pic.webformatURL} alt="" className="w-full h-56" />
       <div className="px-6 py-2">
+        {/* .pageUrl */}
         <div className="font-bold text-purple-500 text-xl my-2">Photo by {props.pic.user}</div>
         <ul>
           <li>
@@ -36,15 +37,26 @@ const Cards = (props) => {
           #{tags[2]}
         </span>
       </div>
-      <a
-        href={props.pic.webformatURL}
-        target="_blank"
-        class="inline-flex items-center justify-center w-1/4 h-12 px-6 font-medium m-5 tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none bg-purple-700 hover:bg-orange-500"
-        aria-label="Sign up"
-        title="Sign up"
-      >
-        View
-      </a>
+      <div className="m-0 p-0">
+        <a
+          href={props.pic.webformatURL}
+          target="_blank"
+          class="inline-flex items-center justify-center w-1/3 h-12 px-3 font-medium m-5 tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none bg-purple-700 hover:bg-blue-500 text-md"
+          aria-label="Sign up"
+          title="Sign up"
+        >
+          View in HD
+        </a>
+        <a
+          href={props.pic.pageURL}
+          target="_blank"
+          class="inline-flex items-center justify-center w-1/4 h-12 px-6 font-medium m-5 ml-0 tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none bg-orange-500 hover:bg-blue-500"
+          aria-label="Sign up"
+          title="Sign up"
+        >
+          Website
+        </a>
+      </div>
     </div>
   );
 };
